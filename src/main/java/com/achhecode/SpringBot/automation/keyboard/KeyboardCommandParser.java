d133a666a5d0669e0451537629219bca45e15d74
A R Danish <helloardanish@gmail.com>
2026-09-25 23:17:33 +0530

    Code refactor

--- src/main/java/com/achhecode/SpringBot/automation/keyboard/KeyboardCommandParser.java
+++ src/main/java/com/achhecode/SpringBot/automation/keyboard/KeyboardCommandParser.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Component
 public class KeyboardCommandParser {
 
+    @SuppressWarnings("null")
     public List<KeyboardCommand> parse(String input) {
 
         if (input == null || input.isBlank()) {
